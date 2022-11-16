40a39df20d07d953a772c7ce3124ad2f6e798fe3
iqra-apps-studio <emon08ctg@gmail.com>
2022-11-16 18:06:13 +0600

    New Work Drawer Menu Doner Drawer Menu User

--- app/src/main/java/com/iqrastudio/smartzakatsolution/Donor_Profile_Dashboard.java
+++ app/src/main/java/com/iqrastudio/smartzakatsolution/Donor_Profile_Dashboard.java
@@ -1,14 +1,35 @@
 package com.iqrastudio.smartzakatsolution;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Donor_Profile_Dashboard extends AppCompatActivity {
+
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donor_profile_dashboard);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_lay);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+      //  navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
